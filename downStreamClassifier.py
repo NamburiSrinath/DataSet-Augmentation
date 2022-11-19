0c03562e1412ddc4ce40c59a4efd03ac18151af3
NamburiSrinath <namburisrinath@gmail.com>
2022-11-19 13:45:29 -0600

    Classification code latest

--- downStreamClassifier.py
+++ downStreamClassifier.py
@@ -10,13 +10,12 @@ import torch.optim as optim
 from torch.utils.data import random_split
 import matplotlib.pyplot as plt
 import math
-
-
 import random
 import logging
 import os
 from datetime import datetime
 import numpy as np
+from torch.utils.tensorboard import SummaryWriter
 
 torch.manual_seed(0)
 random.seed(0)
@@ -24,9 +23,30 @@ np.random.seed(0)
 g = torch.Generator()
 g.manual_seed(0)
 
+# writer = SummaryWriter("../tensorboard_logs")
+
 if not os.path.exists("logs"):
     os.mkdir("logs")
-logging.basicConfig(filename='logs/prompting_log.log', format='%(asctime)s %(message)s', level=logging.INFO)
+logging.basicConfig(filename='logs/experiment_50epochs.log', format='%(asctime)s %(message)s', level=logging.INFO)
+
+train_val_ratio = 0.85
+no_of_epochs = 50
+batch_size = 512
+num_classes = 10
+pretrained = False 
+tunable = True
+logging.info(f'No of epochs: {no_of_epochs}, pretrained: {pretrained}, tunable: {tunable}')
+
+classes = ['airplane', 'automobile', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+label_list = ['airplane', 'automobile', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+# train_folder = '/hdd2/srinath/dataset_augmentation_diffusers/train_images/'
+train_folder = '/hdd2/srinath/Dreambooth-Stable-Diffusion/generated_images'
+# test_folder = '/hdd2/srinath/Imge_net_images'
+test_folder = '/hdd2/srinath/dataset_augmentation_diffusers/imagenet_val/'
 
 # ImageNet transformer
 transform = transforms.Compose([
@@ -35,24 +55,39 @@ transform = transforms.Compose([
     ])
 
 # Generated data transformer
-data_transform = transforms.Compose([
+training_data_transform = transforms.Compose([
         transforms.RandAugment(magnitude=15),
         transforms.Resize((256, 256)),
         transforms.ToTensor(),
     ])
+# class AverageMeter(object):
+#     """Computes and stores the average and current value"""
 
-batch_size = 32
-num_classes = 10
+#     def __init__(self):
+#         self.reset()
 
-classes = ['airplane', 'automobile', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+#     def reset(self):
+#         self.val = 0.0
+#         self.avg = 0.0
+#         self.sum = 0
+#         self.count = 0.0
 
-label_list = ['airplane', 'automobile', 'bird', 'cat',
-           'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+#     def update(self, val, n=1):
+#         self.val = val
+#         self.sum += val * n
+#         self.count += n
+#         self.avg = self.sum / self.count
 
-def train_validation_test_splits(train_folder, test_folder):
-    train_set = datasets.ImageFolder(root=train_folder, transform=transform)
-    trainset_size = math.ceil(len(train_set) * 0.85)
+def train_validation_test_splits(train_folder, test_folder, train_val_ratio, train_transform = transform):
+    '''
+    Given train and test folders, split the dataset into train, validation and test sets
+    Train and Validation data is splitted in the train_val_ratio
+
+    Returns:
+        Train, Validation and test data loader
+    '''
+    train_set = datasets.ImageFolder(root=train_folder, transform=train_transform)
+    trainset_size = math.ceil(len(train_set) * train_val_ratio)
     valset_size = len(train_set) - trainset_size
     trainset, validationset = random_split(train_set, [trainset_size, valset_size])  
 
@@ -68,35 +103,74 @@ def train_validation_test_splits(train_folder, test_folder):
     test_dataloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                             shuffle=True, num_workers=2)
     logging.info('Test set: ImageNet')
-
     return train_dataloader, validation_dataloader, test_dataloader
 
-def train(net, train_loader, no_of_epochs):
+def set_parameter_requires_grad(model, tunable):
+	'''
+    	We would either want to 
+    	    1. Freeze the backbone i.e tunable = False -> requires_grad = False
+    	    2. Tunable backbone i.e tunable = True -> requires_grad = True
+    	
+    	Returns: 
+    	    The model with either frozen or tunable backbone with a Linear head
+    	'''
+	for param in model.parameters():
+		param.requires_grad = tunable
+	model.fc = nn.Linear(512, num_classes)
+	return model
+
+def verify_freeze(model):
+    '''
+    Verify whether the backbone is frozen or tunable
+
+    Returns:
+        1. params_to_update -> Parameters to update which is used by Optimizer
+        2. Layers to update -> Names of layers for which backpropagation occurs (just to verify)
+    '''
+    params_to_update = []
+    layers_update = []
+    for name, param in model.named_parameters():
+        if param.requires_grad == True:
+            params_to_update.append(param)
+            layers_update.append(name)
+    return params_to_update, layers_update
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].reshape(-1).float().sum(0)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
+
+def train(net, train_loader, no_of_epochs, criterion, optimizer):
+
+    # batch_time = AverageMeter()
+    # data_time = AverageMeter()
+    # losses = AverageMeter()
+    # top1 = AverageMeter()
 
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
     net.to(device)
-
-    criterion = nn.CrossEntropyLoss()
-    print(net.parameters())
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-    # num_batches = trainset_size // batch_size
     num_batches = len(train_loader)
 
     for epoch in range(no_of_epochs):  # loop over the dataset multiple times
-
         correct = 0
         total = 0
         running_loss = 0.0
         
         for i, data in enumerate(train_loader, 0):
-
             # get the inputs; data is a list of [inputs, labels]
             # inputs, labels = data
             inputs, labels = data[0].to(device), data[1].to(device)
-            
-            # print label
-            # print(labels)
 
             # zero the parameter gradients
             optimizer.zero_grad()
@@ -104,21 +178,31 @@ def train(net, train_loader, no_of_epochs):
             # forward + backward + optimize
             outputs = net(inputs)
             loss = criterion(outputs, labels)
+
+            # acc1, acc5 = accuracy(output, target, topk=(1, 5))
+            # losses.update(loss.item(), input.size(0))
+            # top1.update(acc1.item(), input.size(0))
+            # top5.update(acc5.item(), input.size(0))
+
             loss.backward()
             optimizer.step()
 
             # print statistics
             running_loss += loss.item()
             
-            if i == (num_batches-1):    # print every 2000 mini-batches
-                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 500:.3f}')
-                logging.info(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 500:.3f}')
+            if i == (num_batches - 1):    # print at the end of batch
+                print(f'[Epoch: {epoch + 1}, Batch: {i + 1:5d}] loss: {running_loss / num_batches:.3f}')
+                logging.info(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / num_batches:.3f}')
                 running_loss = 0.0
                 _, predicted = torch.max(outputs.data.cuda(), 1)
                 total += labels.size(0)
                 correct += (predicted == labels.cuda()).sum().item()
-                print(f'Accuracy: {100 * correct // total} %')
-                logging.info(f'Accuracy: {100 * correct // total} %')
+                print(f'Training Accuracy: {100 * correct // total} %')
+                logging.info(f'Training Accuracy: {100 * correct // total} %')
+
+        #         writer.add_scalar("data/training_loss", losses.val, i)
+        # writer.add_scalars("data/top1_accuracy", {"train": top1.avg}, epoch + 1)
+        # writer.add_scalars("data/top5_accuracy", {"train": top5.avg}, epoch + 1)
 
     print('Finished Training')
     logging.info('Finished Training')
@@ -132,7 +216,7 @@ def train(net, train_loader, no_of_epochs):
 def test(net, test_loader):
     correct = 0
     total = 0
-
+    net.eval()
     with torch.no_grad():
         for data in test_loader:
             images, labels = data
@@ -169,54 +253,29 @@ def test(net, test_loader):
         print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
         logging.info(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
 
-# Observe that all parameters are being optimized
-optimizer = optim.SGD(params_to_update, lr=0.001, momentum=0.9)
-criterion = nn.CrossEntropyLoss()
+if __name__ == "__main__":
+    # Get dataloaders
+    train_dataloader, validation_dataloader, test_dataloader = train_validation_test_splits(train_folder, test_folder, 
+                                                                                train_val_ratio, training_data_transform)
 
+    # Get model
+    model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=pretrained)              
+    model = set_parameter_requires_grad(model, tunable)
+    model = model.to('cuda')
+    params_to_update, layers_update = verify_freeze(model)
+    print("No of layers backprop is going is :", len(layers_update))
 
-no_of_epochs = 3
+    # Initialize optimizer and loss function
+    optimizer = optim.SGD(params_to_update, lr=0.001, momentum=0.9)
+    criterion = nn.CrossEntropyLoss()
 
-# train_folder = '/hdd2/srinath/dataset_augmentation_diffusers/train_images/'
-train_folder = '/hdd2/srinath/Dreambooth-Stable-Diffusion/generated_images'
-test_folder = '/hdd2/srinath/Imge_net_images'
+    # Train
+    train(model, train_dataloader, no_of_epochs, criterion, optimizer)
 
-train_dataloader, validation_dataloader, test_dataloader = train_validation_test_splits(train_folder, test_folder)
+    print("------ Validation starts-----", train_folder)
+    logging.info(f"Validation on {train_folder} test set:")
+    test(model, validation_dataloader)
 
-
-# We will just finetune (so won't pass gradient to back)
-def set_parameter_requires_grad(model, feature_extract):
-    if feature_extracting:
-        for param in model.parameters():
-            param.requires_grad = False
-
-model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=True)
-feature_extract = True                 
-set_parameter_requires_grad(model, feature_extract)
-model.fc = nn.Linear(512, num_classes)
-
-model = model.to('cuda')
-
-params_to_update = model.parameters()
-
-print("Params to learn:")
-if feature_extract:
-    params_to_update = []
-    for name,param in model.named_parameters():
-        if param.requires_grad == True:
-            params_to_update.append(param)
-            print("\t",name)
-else:
-    for name,param in model.named_parameters():
-        if param.requires_grad == True:
-            print("\t",name)
-
-
-train(model, train_dataloader, no_of_epochs)
-
-print("Test on Validation data i.e :", train_folder)
-logging.info(f"Test on {train_folder} test set:")
-test(model, validation_dataloader)
-
-print("Test on Test dataset i.e :", test_folder)
-logging.info(f"Test on {test_folder} test set:")
-test(model, test_dataloader)
+    print("------ Test starts-----", test_folder)
+    logging.info(f"Test on {test_folder} test set:")
+    test(model, test_dataloader)
